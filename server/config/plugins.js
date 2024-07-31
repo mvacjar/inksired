1416adbeea7736db54ee283e5d8853b340f5d871
mvacjar <mvacjar@gmail.com>
2024-07-31 11:56:03 +0200

    credentials in .env

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -1,1 +1,21 @@
-module.exports = () => ({});
+require("dotenv").config();
+
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+          },
+          region: env("AWS_REGION"),
+          params: {
+            Bucket: env("AWS_BUCKET_NAME"),
+          },
+        },
+      },
+    },
+  },
+});
