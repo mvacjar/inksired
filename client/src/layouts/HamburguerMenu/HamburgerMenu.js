aaab9e9fdde06e884b9bfe7d985680d8027f7aa1
mvacjar <mvacjar@gmail.com>
2024-08-09 13:58:37 +0200

    Hamburguer Menu Design Done

--- client/src/layouts/HamburguerMenu/HamburgerMenu.js
+++ client/src/layouts/HamburguerMenu/HamburgerMenu.js
@@ -1,49 +1,72 @@
+import { useState, useRef, useEffect } from 'react';
 import styles from './hamburgerMenu.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
 
 export default function HamburgerMenu() {
   const [openMenu, setOpenMenu] = useState(false);
-  const [isSpinning, setIsSpinning] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setOpenMenu(!openMenu);
-    setIsSpinning(true);
-    setTimeout(() => setIsSpinning(false), 500);
   };
 
-  return (
-    <>
-      <div className={styles.hamburgerMenu}>
-        <Image
-          src='/images/logo.svg'
-          width={45}
-          height={45}
-          alt='icon-menu'
-          className={`${styles.logo} ${isSpinning ? styles.spin : ''}`}
-          onClick={toggleMenu}
-        />
-        {openMenu && (
-          <nav className={styles.dropdownMenu}>
-            <section className={styles.menuContainer}>
-              <Link href='#' className={styles.linkTop}>
-                Profile
-              </Link>
-              <span className={styles.line}></span>
+  useEffect(() => {
+    const menu = menuRef.current;
 
-              <Link href='#' className={styles.link}>
-                Contact
-              </Link>
-              <span className={styles.line}></span>
-              <Link href='' className={styles.linkBottom}>
-                Logout
-              </Link>
-              <span className={styles.line}></span>
-            </section>
-          </nav>
-        )}
-      </div>
-    </>
+    const handleAnimationEnd = () => {
+      if (!openMenu) {
+        menu.style.display = 'none';
+      }
+    };
+
+    menu.addEventListener('animationend', handleAnimationEnd);
+
+    return () => {
+      menu.removeEventListener('animationend', handleAnimationEnd);
+    };
+  }, [openMenu]);
+
+  useEffect(() => {
+    if (openMenu) {
+      menuRef.current.style.display = 'block';
+    }
+  }, [openMenu]);
+
+  return (
+    <div className={styles.hamburgerMenu}>
+      <Image
+        src='/images/logo.svg'
+        width={45}
+        height={45}
+        alt='icon-menu'
+        className={`${styles.logo} ${
+          openMenu ? styles.spinRight : styles.spinLeft
+        }`}
+        onClick={toggleMenu}
+      />
+
+      <nav
+        ref={menuRef}
+        className={`${styles.dropdownMenu} ${
+          openMenu ? styles.slideRight : styles.slideLeft
+        }`}
+      >
+        <section className={styles.menuContainer}>
+          <Link href='#' className={styles.linkTop}>
+            Profile
+          </Link>
+          <span className={styles.line}></span>
+          <Link href='#' className={styles.link}>
+            Contact
+          </Link>
+          <span className={styles.line}></span>
+          <Link href='' className={styles.linkBottom}>
+            Logout
+          </Link>
+          <span className={styles.line}></span>
+        </section>
+      </nav>
+    </div>
   );
 }
