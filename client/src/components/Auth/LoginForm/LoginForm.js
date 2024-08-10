f80e56bec5a5e6354b8d23df22c344d57d7de41e
mvacjar <mvacjar@gmail.com>
2024-08-10 11:39:49 +0200

    Update some animations and clean scss

--- client/src/components/Auth/LoginForm/LoginForm.js
+++ client/src/components/Auth/LoginForm/LoginForm.js
@@ -67,7 +67,7 @@ export default function LoginForm() {
         )}
 
         <button
-          className={styles.buttonSignIn}
+          className={styles.signInButton}
           type='submit'
           disabled={formik.isSubmitting}
         >
