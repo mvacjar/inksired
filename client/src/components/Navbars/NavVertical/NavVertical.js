e2700dc98a285c5da8d9cf28861a774dbf94ef83
mvacjar <mvacjar@gmail.com>
2024-08-10 21:04:33 +0200

    bookmark form in the vertical nav

--- client/src/components/Navbars/NavVertical/NavVertical.js
+++ client/src/components/Navbars/NavVertical/NavVertical.js
@@ -6,63 +6,66 @@ import HamburgerMenu from '@/components/Navbars/HamburguerMenu/HamburgerMenu';
 export default function Navbar() {
   return (
     <>
-      <nav className={styles.navbar}>
-        <div className={styles.menuContainer}>
-          <div className={styles.logoContainer}>
-            <HamburgerMenu />
+      <div className={styles.navbarContainer}>
+        <nav className={styles.navbar}>
+          <div className={styles.bookmarkForm}></div>
+          <div className={styles.menuContainer}>
+            <div className={styles.logoContainer}>
+              <HamburgerMenu />
+            </div>
+            <div className={styles.linksContainer}>
+              <Link href='/profile' className={styles.links}>
+                <Image
+                  src='/images/home.svg'
+                  width={35}
+                  height={35}
+                  alt='icon-menu'
+                  className={styles.link}
+                />
+              </Link>
+              <Link href='/bookshelf'>
+                <Image
+                  src='/images/shelf.svg'
+                  width={35}
+                  height={35}
+                  alt='icon-menu'
+                  className={styles.link}
+                />
+              </Link>
+              <Link href='/calification'>
+                <Image
+                  src='/images/star.svg'
+                  width={35}
+                  height={35}
+                  alt='icon-menu'
+                  className={styles.link}
+                />
+              </Link>
+              <Link href='/forum'>
+                <Image
+                  src='/images/forum.png'
+                  width={35}
+                  height={40}
+                  alt='icon-menu'
+                  className={styles.link}
+                />
+              </Link>
+            </div>
+            <div className={styles.titleNavContainer}>
+              <Link href='/home'>
+                <Image
+                  src='/images/logo_dark.svg'
+                  width={120}
+                  height={120}
+                  alt='logo-menu'
+                  priority
+                  className={styles.titleNav}
+                />
+              </Link>
+            </div>
           </div>
-          <div className={styles.linksContainer}>
-            <Link href='/profile' className={styles.links}>
-              <Image
-                src='/images/home.svg'
-                width={35}
-                height={35}
-                alt='icon-menu'
-                className={styles.link}
-              />
-            </Link>
-            <Link href='/bookshelf'>
-              <Image
-                src='/images/shelf.svg'
-                width={35}
-                height={35}
-                alt='icon-menu'
-                className={styles.link}
-              />
-            </Link>
-            <Link href='/calification'>
-              <Image
-                src='/images/star.svg'
-                width={35}
-                height={35}
-                alt='icon-menu'
-                className={styles.link}
-              />
-            </Link>
-            <Link href='/forum'>
-              <Image
-                src='/images/forum.png'
-                width={35}
-                height={40}
-                alt='icon-menu'
-                className={styles.link}
-              />
-            </Link>
-          </div>
-          <div className={styles.titleNavContainer}>
-            <Link href='/home'>
-              <Image
-                src='/images/logo_dark.svg'
-                width={120}
-                height={120}
-                alt='logo-menu'
-                priority
-                className={styles.titleNav}
-              />
-            </Link>
-          </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
   );
 }
