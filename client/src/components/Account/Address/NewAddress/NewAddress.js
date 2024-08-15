57eb6e043aff8d16895f2911efeed1165ec775d6
mvacjar <mvacjar@gmail.com>
2024-08-15 15:12:04 +0200

    Modal included to create a new address

--- client/src/components/Account/Address/NewAddress/NewAddress.js
+++ client/src/components/Account/Address/NewAddress/NewAddress.js
@@ -1,20 +1,22 @@
-import styles from './newAddress.module.scss';
 import { BasicModal } from '@/components/Shared/BasicModal/BasicModal';
 import { AddressForm } from '../AddressForm';
 import { useState } from 'react';
 
-export function NewAddress() {
-  const [show, setShow] = useState(false);
+export function NewAddress(props) {
+  const [open, setOpen] = useState(false);
+  const { onReload } = props;
 
-  const onOpenClose = (e) => {
-    e.preventDefault();
-    setShow((prevState) => !prevState);
+  const handleOpenClose = (shouldOpen) => {
+    setOpen(shouldOpen);
   };
 
   return (
     <>
-      <BasicModal show={show} onOpenClose={onOpenClose}>
-        <AddressForm onClose={onOpenClose} />
+      <BasicModal open={open} onClose={handleOpenClose} title='New Address'>
+        <AddressForm
+          onClose={() => handleOpenClose(false)}
+          onReload={onReload}
+        />
       </BasicModal>
     </>
   );
