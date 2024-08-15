5b4d66f4fb6616c24d64c5cb1cdc35f984e385c0
mvacjar <mvacjar@gmail.com>
2024-08-15 16:43:10 +0200

    Change button created to another place to void repetition

--- client/src/components/Account/Address/NewAddress/NewAddress.js
+++ client/src/components/Account/Address/NewAddress/NewAddress.js
@@ -1,6 +1,7 @@
 import { BasicModal } from '@/components/Shared/BasicModal/BasicModal';
 import { AddressForm } from '../AddressForm';
 import { useState } from 'react';
+import Button from '@mui/material/Button';
 
 export function NewAddress(props) {
   const [open, setOpen] = useState(false);
@@ -10,8 +11,32 @@ export function NewAddress(props) {
     setOpen(shouldOpen);
   };
 
+  const styleButton = {
+    display: 'inline-block',
+    bgcolor: '#96503e',
+    color: '#fffbef',
+    borderRadius: 10,
+    fontSize: 13,
+    border: 'none',
+    cursor: 'pointer',
+    padding: '0.5rem 1rem',
+    marginTop: '0',
+    whiteSpace: 'nowrap',
+    transition: 'transform ease 0.2s',
+    width: 'auto',
+
+    '&:hover': {
+      backgroundColor: '#96503e',
+      transform: 'scale(1.05)',
+    },
+  };
+
   return (
     <>
+      <Button onClick={() => handleOpenClose(true)} sx={styleButton}>
+        Create
+      </Button>
+
       <BasicModal open={open} onClose={handleOpenClose} title='New Address'>
         <AddressForm
           onClose={() => handleOpenClose(false)}
--- client/src/components/Shared/BasicModal/BasicModal.js
+++ client/src/components/Shared/BasicModal/BasicModal.js
@@ -1,5 +1,4 @@
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
@@ -23,53 +22,22 @@ const styleModal = {
   borderRadius: 3,
   boxShadow: 14,
   p: 3,
-  '&:active': {
-    border: 'none',
-  },
   '@media (max-width: 768px)': {
     width: '80%',
   },
 };
 
-const styleButton = {
-  bgcolor: '#96503e',
-  color: '#fffbef',
-  borderRadius: 10,
-  fontSize: 13,
-  border: 'none',
-  cursor: 'pointer',
-  padding: '0.5rem 1rem',
-  marginTop: '0',
-  whiteSpace: 'nowrap',
-  textTransform: 'none',
-  transition: 'transform ease 0.2s',
-  '&:hover': {
-    bgcolor: '#96503e',
-    transform: 'scale(1.05)',
-  },
-};
 export function BasicModal({ open, onClose, children, title }) {
   return (
-    <div>
-      <div
-        style={{
-          display: 'flex',
-          alignContent: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Button onClick={() => onClose(true)} sx={styleButton}>
-          Create
-        </Button>
-      </div>
-      <Modal open={open} onClose={() => onClose(false)}>
-        <Box sx={styleModal}>
-          <Typography variant='h6' component='h2' sx={styleTitle}>
-            {title}
-          </Typography>
-          <Typography sx={{ mt: 2 }}>{children}</Typography>
-        </Box>
-      </Modal>
-    </div>
+    <Modal open={open} onClose={() => onClose(false)}>
+      <Box sx={styleModal}>
+        <Typography variant='h6' component='h2' sx={styleTitle}>
+          {title}
+        </Typography>
+        <Typography variant='div' sx={{ mt: 2 }}>
+          {children}
+        </Typography>
+      </Box>
+    </Modal>
   );
 }
