bcccb645a2466247423fca71521c75fb0478dbf5
mvacjar <mvacjar@gmail.com>
2024-08-21 16:59:27 +0200

    api to render few books

--- client/src/api/book.js
+++ client/src/api/book.js
@@ -19,4 +19,30 @@ export class Book {
       throw error;
     }
   }
+
+  async getLatestBooks({ limit = 10, literaryGenresId = null }) {
+    try {
+      const pagination = `pagination[limit]=${limit}`;
+      const sort = `sort[0]=publishedAt:desc`;
+      const populate = `populate=*`;
+      const filterLiteraryGenres =
+        literaryGenresId &&
+        `filters[literary-genres][id][$eq]=${literaryGenresId}`;
+
+      const urlParams = [sort, pagination, filterLiteraryGenres, populate]
+        .filter((param) => param)
+        .join('&');
+
+      const url = `${ENV.API_URL}/${ENV.ENDPOINTS.BOOKS}?${urlParams}`;
+
+      const response = await fetch(url);
+      const result = await response.json();
+
+      if (response.status !== 200) throw result;
+
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
 }
