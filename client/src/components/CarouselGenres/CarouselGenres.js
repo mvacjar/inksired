c770215216e473fb76548345f3c8056cf4ec24e8
mvacjar <mvacjar@gmail.com>
2024-08-22 17:27:14 +0200

    Change the style of the Carousel of Genres
    where it will show the arrows with a linear background in each side, depending of the % that we are in the carousel

--- client/src/components/CarouselGenres/CarouselGenres.js
+++ client/src/components/CarouselGenres/CarouselGenres.js
@@ -1,7 +1,9 @@
 import styles from './carouselGenres.module.scss';
 import { map } from 'lodash';
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { useState, useEffect, useRef } from 'react';
 import { LiteraryGenres } from '@/api';
 
 const genresCtrl = new LiteraryGenres();
@@ -9,6 +11,9 @@ const genresCtrl = new LiteraryGenres();
 export default function CarouselGenres(props) {
   const { isOpenSearch } = props;
   const [genres, setGenres] = useState(null);
+  const [showLeftArrow, setShowLeftArrow] = useState(false);
+  const [showRightArrow, setShowRightArrow] = useState(true);
+  const carouselRef = useRef(null);
 
   useEffect(() => {
     (async () => {
@@ -21,21 +26,61 @@ export default function CarouselGenres(props) {
     })();
   }, []);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const carousel = carouselRef.current;
+      const scrollLeft = carousel.scrollLeft;
+      const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
+
+      setShowLeftArrow(scrollLeft > 0);
+      setShowRightArrow(scrollLeft < maxScrollLeft);
+    };
+
+    const carousel = carouselRef.current;
+    carousel.addEventListener('scroll', handleScroll);
+
+    return () => {
+      carousel.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={styles.carouselContainer}>
-      <section className={styles.carousel}>
-        <div className={styles.carouselItems}>
-          {map(genres, (genre) => (
-            <Link
-              key={genre.id}
-              href={`/genres/${genre.attributes.slug_genres}`}
-              className={styles.carouselItem}
-            >
-              {genre.attributes.title}
-            </Link>
-          ))}
+    <>
+      <div className={styles.genresWrapper}>
+        <div className={styles.genresContent}>
+          {showLeftArrow && (
+            <div className={styles.leftArrowWrapper}>
+              <div className={styles.leftArrowContainer}>
+                <ArrowBackIosIcon
+                  sx={{ color: '#2d2d2d', fontSize: 25 }}
+                  className={styles.leftArrow}
+                />
+              </div>
+            </div>
+          )}
+          <div className={styles.carouselItems} ref={carouselRef}>
+            {map(genres, (genre) => (
+              <Link
+                key={genre.id}
+                href={`/genres/${genre.attributes.slug_genres}`}
+                className={styles.carouselItem}
+              >
+                {genre.attributes.title}
+              </Link>
+            ))}
+          </div>
+          {showRightArrow && (
+            <div className={styles.rightArrowWrapper}>
+              <div className={styles.rightArrowContainer}>
+                <ArrowForwardIosIcon
+                  sx={{ color: '#2d2d2d', fontSize: 25 }}
+                  className={styles.rightArrow}
+                />
+              </div>
+            </div>
+          )}
         </div>
-      </section>
-    </div>
+      </div>
+    </>
   );
 }
