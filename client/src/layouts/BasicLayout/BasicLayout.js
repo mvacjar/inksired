599dcaa11ab73b44d71be252fed2ca858d08206e
mvacjar <mvacjar@gmail.com>
2024-08-23 19:42:41 +0200

    Basic Layouts with the navbars

---
+++ client/src/layouts/BasicLayout/BasicLayout.js
@@ -0,0 +1,11 @@
+import NavHorizontal from '@/components/Navbars/NavHorizontal/NavHorizontal';
+import NavVertical from '@/components/Navbars/NavVertical/NavVertical';
+
+export function BasicLayout() {
+  return (
+    <>
+      <NavVertical />
+      <NavHorizontal />
+    </>
+  );
+}
