ac44e81f846f37868c92c60d95e2a2f01c65a181
mvacjar <mvacjar@gmail.com>
2024-08-27 10:43:17 +0200

    Create profile-icon.js

---
+++ server/src/api/profile-icon/services/profile-icon.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * profile-icon service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::profile-icon.profile-icon');
